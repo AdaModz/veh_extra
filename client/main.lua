35d858b7e1a1b8c6d1563b20515abaf572dc6e22
nearbyplayer <aradz1299@gmail.com>
2018-08-26 12:03:45 -0400

    ES5 Update

--- client/main.lua
+++ client/main.lua
@@ -8,12 +8,12 @@
 
 RegisterNetEvent('car:vehicleExtras')
 AddEventHandler('car:vehicleExtras', function(extraId, toggle)
-  local veh = GetVehiclePedIsIn(GetPlayerPed(-1))
-  if IsPedInAnyVehicle(GetPlayerPed(-1), false) then
-    if GetPedInVehicleSeat(veh, -1) == GetPlayerPed(-1) then
-      if DoesExtraExist(veh, extraId) then
-        SetVehicleExtra(veh, extraId, toggle)
-      end
-    end
-  end
+	local veh = GetVehiclePedIsIn(GetPlayerPed(-1))
+	if IsPedInAnyVehicle(GetPlayerPed(-1), false) then
+		if GetPedInVehicleSeat(veh, -1) == GetPlayerPed(-1) then
+			if DoesExtraExist(veh, extraId) then
+				SetVehicleExtra(veh, extraId, toggle)
+			end
+		end
+	end
 end)
--- server/main.lua
+++ server/main.lua
@@ -6,13 +6,13 @@
 --]]
 
 TriggerEvent('es:addCommand', 'extra', function(source, args, user)
-  local extraId = tonumber(args[2])
-  local toggle = tonumber(args[3])
-  if toggle == 1 then
-    toggle = false
-    TriggerClientEvent('car:vehicleExtras', source, extraId, toggle)
-  elseif toggle == 0 then
-    toggle = true
-    TriggerClientEvent('car:vehicleExtras', source, extraId, toggle)
-  end
+	local extraId = tonumber(args[1])
+	local toggle = tonumber(args[2])
+	if toggle == 1 then
+		toggle = false
+		TriggerClientEvent('car:vehicleExtras', source, extraId, toggle)
+	elseif toggle == 0 then
+		toggle = true
+		TriggerClientEvent('car:vehicleExtras', source, extraId, toggle)
+	end
 end, {help = 'Vehicle Extras', params = {{name = 'id', help = 'Extra ID'}, {name = 'toggle', help = '0 (Off), 1 (On)'}}})
